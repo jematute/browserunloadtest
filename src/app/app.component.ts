2216e47fbeb9bd114806e724785c55af4b90c817
Juan Matute <juan.matute@gmail.com>
2022-03-10 08:57:46 -0500

    testing unload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, HostListener } from '@angular/core';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'browserunload';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  confirmLeavingPageBeforeSaving(eventClose: Event): boolean {
+    eventClose.preventDefault();
+    return false; 
+  }
+
+  @HostListener('window:unload', ['$event'])
+  saveBeforeLeaving(eventClose: Event) {
+    this.http.get("http://localhost:5000/test").pipe(switchMap((data) => {
+      console.log("data");
+      return this.http.get("http://localhost:5000/data")
+    })).subscribe((a) => {
+      console.log(a);
+      console.log("done!")
+    })
+    
+
+  }
 }
